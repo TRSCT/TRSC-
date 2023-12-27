2d9bef8505777bb08a007fb66ef38c5a284e7d6c
TRSC <154905352+TRSCT@users.noreply.github.com>
2023-12-27 18:51:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// //เหมือนกับprintในไพทอน= print("Hello Wolrd")
+// console.log("Hello World")
+
+//  //การประกาศตัวแปร//
+
+
+
+// console.log(9+1)
+
+
+// //การทำซ้ำ
+// for (let i =1; i < 10;i++){
+//     console.log("ครั้งที่ " +i)
+// }
+
+
+
+let A = ["a","b","c","d","e"]
+
+// function Array(){
+//     console.log("Hello world")
+// }
+
+
+// Array()
+
+
+
+A.forEach((value) => {
+    console.log(value)
+})
+

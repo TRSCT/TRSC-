95afe92ff427df5d410c6ebb990c25585c2155de
TRSC <154905352+TRSCT@users.noreply.github.com>
2024-01-02 18:34:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// //เหมือนกับprintในไพทอน= print("Hello Wolrd")
-// console.log("Hello World")
+// เหมือนกับprintในไพทอน= print("Hello Wolrd")
+//  console.log("Hello World")
 
-//  //การประกาศตัวแปร//
+// //  //การประกาศตัวแปร//
 
 
 
@@ -9,24 +9,46 @@
 
 
 // //การทำซ้ำ
-// for (let i =1; i < 10;i++){
-//     console.log("ครั้งที่ " +i)
-// }
+//  for (let i =1; i < 10;i++){
+//      console.log("ครั้งที่ " +i)
+//  }
 
 
 
-let A = ["a","b","c","d","e"]
+// let A = ["a","b","c","d","e"]
 
 // function Array(){
-//     console.log("Hello world")
+//      console.log("Hello world")
+//  }
+
+
+//  Array()
+// let doc = {
+//     name : "jonh",
+//     lastname : "Doe",
+//     age : 20,
+//     address : "USA",
+//     phone : "1212121212",
+//     height : 155.51,
 // }
 
 
-// Array()
+// A.forEach((value) => {
+//     console.log(value)
+// })
+
+// let i = 0
+// while (i <= 10){
+//     console.log("ครั้งที่" + i)
+//     i++
+// }
 
 
-
-A.forEach((value) => {
-    console.log(value)
-})
-
+let age = 80
+if(age < 20) {
+    console.log("อายุน้อยกว่า 20")
+} else if (age > 20 && age < 50) {
+    console.log("อายุมากกว่า 20")
+} else {
+    console.log("คุณกรอกข้อมูลผิดพลาด")
+}
